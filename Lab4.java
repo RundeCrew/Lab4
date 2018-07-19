857d05ff4317b686b15e7e120f762008796946e4
RundeCrew <kris.runde@gmail.com>
2018-07-18 22:54:00 -0400

    added more comments

--- Lab4.java
+++ Lab4.java
@@ -8,25 +8,25 @@ public class Lab4 {
 		//ask for a number
 		char answer = 'y';
 		
-		System.out.println("Learn your squared and cubes!\n");
+		System.out.println("Learn your squared and cubes!\n"); // Title of Table
 		
 		do {
 			
-			System.out.println("Please enter an integer: ");
+			System.out.println("Please enter an integer: "); //ask for user to input a number
 			int userNum = scnr.nextInt();
 			
-			System.out.println("Number\t" + "Squared\t" + "Cubed");
-			System.out.println("=======\t=======\t=======");
+			System.out.println("Number\t" + "Squared\t" + "Cubed"); // output of Column names
+			System.out.println("=======\t=======\t======="); //output of underlines for column names
 			// loop for the number entered.
 		
-			for (int i = 1 ; i <= userNum ; ++i) {      
-				System.out.println(i + " \t" + ( (int) Math.pow(i, 2)) + " \t" + ((int) Math.pow(i,  3)));
+			for (int i = 1 ; i <= userNum ; ++i) {      //loop as many as number entered 
+				System.out.println(i + " \t" + ( (int) Math.pow(i, 2)) + " \t" + ((int) Math.pow(i,  3))); // mathpow, but mayber there's a library??
 			}
 			
-			System.out.print("\nContinue? (y/n)");
+			System.out.print("\nContinue? (y/n)"); //conditional
 			answer = scnr.next().charAt(0);
 			
-		} while (answer == 'y');
+		} while (answer == 'y'); //while loop
 		
 		
 	}
